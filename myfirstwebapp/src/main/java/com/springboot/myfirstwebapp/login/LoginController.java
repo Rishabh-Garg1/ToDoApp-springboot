d1d8b779e516a40c57e5ef310f17f6ac23cdedbe
Rishabh-Garg1 <r2112g@gmail.com>
2024-08-30 13:34:43 +0530

    initial commit

---
+++ myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,15 @@
+package com.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	// /login => com.springboot.myfirstwebapp.login.LoginController => login.jsp
+	
+	@RequestMapping("login")
+	public String gotoLoginPage() {
+		return "login";
+	}
+	
+}
