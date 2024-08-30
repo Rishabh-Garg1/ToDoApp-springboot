17b93f677536e3e9b36940759433a2f5e815c5c3
Rishabh-Garg1 <r2112g@gmail.com>
2024-08-30 22:14:35 +0530

    3rd commit

--- myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,5 @@
 package com.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-	// /login => com.springboot.myfirstwebapp.login.LoginController => login.jsp
-	// http://localhost:8080/login?name=anyname
-	// Model
-	
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
-		logger.debug("Request param is {}", name);
-		logger.info("I want this printed at info level");
-		logger.warn("I want this printed at warn level");
-		
-		System.out.println("Request param is "+name);
 		return "login";
 	}
 	
