75ddcaed875b3e0fcab90f7a12459fceb91ff84e
Rishabh-Garg1 <r2112g@gmail.com>
2024-09-01 20:58:52 +0530

    commit for 1 sep

--- myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
@@ -1,16 +1,15 @@
 package com.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
+	@RequestMapping(value="login",method = RequestMethod.GET)
+	public String gotoLoginPage() {
+		
 		return "login";
 	}
 	
